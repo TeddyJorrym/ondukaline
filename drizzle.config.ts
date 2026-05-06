0818347ac859a813cbaa3e579a5f98a2e63fe1d9
Teddy Jorrym <teddyjorrym40@gmail.com>
2026-05-06 09:51:17 +0300

    create product details page

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,38 @@ import { loadEnvConfig } from '@next/env'
 loadEnvConfig(cwd())
 
 import { defineConfig } from 'drizzle-kit'
+
+// ✅ Support both env variables
+const connectionString =
+  process.env.DATABASE_URL || process.env.POSTGRES_URL
+
+if (!connectionString) {
+  throw new Error('No database connection string found')
+}
+
 export default defineConfig({
   dialect: 'postgresql',
   schema: './db/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: connectionString,
   },
 })
+
+
+
+
+// import { cwd } from 'node:process'
+// import { loadEnvConfig } from '@next/env'
+
+// loadEnvConfig(cwd())
+
+// import { defineConfig } from 'drizzle-kit'
+// export default defineConfig({
+//   dialect: 'postgresql',
+//   schema: './db/schema.ts',
+//   out: './drizzle',
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// })
